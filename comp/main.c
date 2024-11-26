e8d723ccadff90a5ead4378bebf112e373c930db
Siyi Zheng <million124@163.com>
2024-11-26 12:36:17 -0500

    Initial Commit

---
+++ comp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  comp
+//
+//  Created by 郑斯贻 on 2024-11-26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

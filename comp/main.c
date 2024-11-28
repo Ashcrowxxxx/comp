5b49ecc7faab3c8574dc36b248a0f066a8070003
Siyi Zheng <million124@163.com>
2024-11-27 20:25:16 -0500

    prime numbers between 100 and 200

--- comp/main.c
+++ comp/main.c
@@ -8,13 +8,18 @@
 #include <stdio.h>
 
 int main(void) {
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= i; j++) {
-            int result;
-            result = i * j;
-            printf("%d * %d = %d", i, j, result);
+    
+    for (int num = 100; num <= 200; num++) {
+        int flag = 1;
+        
+        for (int i = 2; i <= (num / 2); i++) {
+            if (num % i == 0) {
+                flag = 0;
+                break;
+            }
+        }if (flag == 1) {
+            printf("%d ", num);
         }
-        printf("\n");
     }
    
     return 0;

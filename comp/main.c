477b7da24f71ce2c8748f8fff1fd585a354d2858
Siyi Zheng <million124@163.com>
2024-11-27 19:58:25 -0500

    Multiplication Table

--- comp/main.c
+++ comp/main.c
@@ -8,12 +8,14 @@
 #include <stdio.h>
 
 int main(void) {
-    int num;
-    
-    for (int num = 1; num <= 100; num++) {
-        if(num % 2 == 1){
-            printf("%d ", num);
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 1; j <= i; j++) {
+            int result;
+            result = i * j;
+            printf("%d * %d = %d", i, j, result);
         }
+        printf("\n");
     }
+   
     return 0;
 }
